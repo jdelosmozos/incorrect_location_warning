18e4c091b96541435e5e48ed25e862d8fc246911
javier <javier@jdelosmozos>
2022-10-20 09:16:25 +0200

    Commit inicial

---
+++ incorrect_location_warning/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Incorrect location warning',
+    'category': 'Inventory',
+    'version': '13.0.1.0',
+    'author': 'Javier L. de los Mozos',
+    'depends': ['stock'],
+    'data': [
+    ],
+    'license': 'Other proprietary',
+    'installable': True,
+    'description' : "The module shows a warning whenever a product unit is tried to be moved from a location diferent as teh picking origin location.",
+}
---
+++ incorrect_location_warning/models/stock_move_line.py
@@ -0,0 +1,20 @@
+from odoo import models, api, _
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+    
+    @api.onchange('lot_id')
+    def onchange_lot_id(self):
+        if self.picking_id.picking_type_id.code in ['outgoing','internal']:
+            if self.product_id.tracking == 'serial':
+                lot_location = self.env['lot.location.report'].browse(self.lot_id.id)
+                if lot_location.posible_current_location:
+                    return {'warning':
+                                {'title': _("Warning! Incorrect product movement"),
+                                 'message': _( "The product is located in two locations: %s, %s" % (lot_location.current_location.name,lot_location.posible_current_location.name))}
+                            }
+                if lot_location.current_location != self.picking_id.location_id:
+                    return {'warning':
+                                {'title': _("Warning! Incorrect product movement"),
+                                 'message': _( "The product is going to be moved from %s but it is located in %s." % (self.picking_id.location_id.name,lot_location.current_location.name))}
+                            }
